d3a52a0a8c9f49f303eb60a49d30472667a61174
Florian Hippenstiel <florian.hippenstiel@web.de>
2022-03-04 17:34:28 +0100

    added eslintrc settings

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
-import { useState } from 'react'
-import './App.css'
-import {ShoppingList} from './Components/ShoppingList.jsx'
+import { useState } from 'react';
+import './App.css';
+import { ShoppingList } from './Components/ShoppingList.jsx';
 
 export function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
-      <header className="App_header">
-        <div className="Shopping_List"><ShoppingList/></div>
-        <div>searchbar</div>
-      </header>
+      <div className="Shopping_List">
+        <ShoppingList />
+      </div>
     </div>
-  )
+  );
 }
-
-
--- src/Components/ShoppingList.jsx
+++ src/Components/ShoppingList.jsx
@@ -1,41 +1,35 @@
 import { useState, useEffect } from 'react';
-import './ShoppingList.css'
+import './ShoppingList.css';
 
-const url='https://fetch-me.vercel.app/api/shopping/items';
+const url = 'https://fetch-me.vercel.app/api/shopping/items';
 
-export function ShoppingList(){
-    const[items,setItems] = useState([]);
+export function ShoppingList() {
+  const [items, setItems] = useState([]);
 
-    useEffect(() =>{
-         async function fetchData (){
-            try{
-                const response = await fetch(url);
-                const data = await response.json();
-                console.log(data.data);
-                setItems(data.data);
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data.data);
+        setItems(data.data);
 
-                return(data.data);
-            }catch(error){
-                console.log(error);
-            }
-        }
-        fetchData();
-    },[])
+        return data.data;
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchData();
+  }, []);
 
-    console.log(items);
-    console.log(items[0].name);
- 
-    return(
-       
-        <ul>
-        {items.map(({ name }) => (
-          <li key={name.de}>{name.de}</li>
-        ))}
-        </ul>
-     )
+  console.log(items);
+  console.log(items[0].name);
+
+  return (
+    <ul>
+      {items.map(({ name }) => (
+        <li key={name.de}>{name.de}</li>
+      ))}
+    </ul>
+  );
 }
-
-
-
-
-

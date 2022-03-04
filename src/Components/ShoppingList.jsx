3601e5a42c871a44408c364d178cb989d81df1f1
Florian Hippenstiel <florian.hippenstiel@web.de>
2022-03-04 15:13:11 +0100

    shoppinglist1

--- src/Components/ShoppingList.jsx
+++ src/Components/ShoppingList.jsx
@@ -4,12 +4,38 @@ import './ShoppingList.css'
 const url='https://fetch-me.vercel.app/api/shopping/items';
 
 export function ShoppingList(){
+    const[items,setItems] = useState([]);
+
     useEffect(() =>{
-        const fetchData
-        
+         async function fetchData (){
+            try{
+                const response = await fetch(url);
+                const data = await response.json();
+                console.log(data.data);
+                setItems(data.data);
+
+                return(data.data);
+            }catch(error){
+                console.log(error);
+            }
+        }
+        fetchData();
     },[])
+
+    console.log(items);
+    console.log(items[0].name);
+ 
+    return(
+       
+        <ul>
+        {items.map(({ name }) => (
+          <li key={name.de}>{name.de}</li>
+        ))}
+        </ul>
+     )
 }
 
-ShoppingList();
+
+
 
 

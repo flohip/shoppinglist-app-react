8cdb28573accce651de42b5d921bd747010c1fb2
Carolin Weitkamp <c.weitkamp@posteo.de>
2022-03-08 00:05:49 +0100

    add cahnge

--- src/Components/ShoppingList.jsx
+++ src/Components/ShoppingList.jsx
@@ -7,6 +7,11 @@ const url='https://fetch-me.vercel.app/api/shopping/items';
 export function ShoppingList(){
     const[items,setItems] = useState([]);
     const[searchItems,setSearchItems] = useState([]);
+    const [addItem, setAddItem] = useState([]);
+
+    useEffect(()=> {
+        setLocalStorage("addedItems", addItem);
+    },[addItem]);
 
     useEffect(() =>{
          async function fetchData (){
@@ -33,21 +38,43 @@ export function ShoppingList(){
         {keySelector: (obj) => obj.name.de},
     ));
     }
+
+    function clickHandler(name,_id){
+        setAddItem([...addItem, name])
+        console.log(name,_id)
+    }
+    console.log(addItem)
     return(
         <>
+        <p> What do you want to bbuy?</p>
         <input
         type="text"
         placeholder="search"
         onChange={fuzzySearch}
         />
-        <ul>
+        <ul className="searched-list">
         {searchItems.map(({ name, _id }) => (
-          <li key={_id}>{name.de}</li>
+          <li onClick={(event)=>clickHandler(name,_id)} className="found-item" key={_id}>{name.de}</li>
         ))}
         </ul>
+        <p> Recently added</p>
+        <ul className="searched-list">
+            {addItem.map((item)=> (
+                <li className="found-item" onClick={(event)=>clickHandler(name,_id)} >{item.de}</li>
+            ))}
+            </ul>
         </>
+
      )
 }
+function getLocalStorage(key){
+    console.log("getLocalStorage is called...");
+    return JSON.parsel(localStorage.getItem(key));
+}
+function setLocalStorage(key, value) {
+    return localStorage.setItem(key, JSON.stringify(value));
+}
+
 
 
 
